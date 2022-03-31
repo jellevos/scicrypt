c4c2d81793cfdb47de6f0efc895cbfd2cef7d1c9
Jelle Vos <github@jelle-vos.nl>
2022-03-31 19:26:01 +0200

    Move id in workflow

--- scicrypt-he/benches/paillier.rs
+++ scicrypt-he/benches/paillier.rs
@@ -11,13 +11,14 @@ use scicrypt_traits::Enrichable;
 
 pub fn paillier_benchmark(c: &mut Criterion) {
     // Ignore noise up to 5%
-    c.noise_threshold(0.05);
+    let mut group = c.benchmark_group("homomorphic_encryption");
+    group.noise_threshold(0.05);
 
     let mut rng = GeneralRng::new(OsRng);
     let (public_key, secret_key) = Paillier::generate_keys(&BitsOfSecurity::AES128, &mut rng);
 
     // Benchmark encryption
-    c.bench_function("paillier_encryption", |b| {
+    group.bench_function("paillier_encryption", |b| {
         b.iter(|| {
             Paillier::encrypt(
                 &Integer::from(black_box(123456789u64)),
