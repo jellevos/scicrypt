58bd17ab7209fc06c54b32f2be5ab08eabd43e7f
Jelle Vos <github@jelle-vos.nl>
2022-05-13 17:19:32 +0200

    Implement pow_mod

--- scicrypt-numbertheory/src/bigint.rs
+++ scicrypt-numbertheory/src/bigint.rs
@@ -2,9 +2,13 @@ use std::{mem::MaybeUninit, ops::{AddAssign, Mul}, alloc::{Layout, System, Globa
 
 use gmp_mpfr_sys::gmp::{self, mpz_t};
 
+const ALIGN: usize = 128;
+const GMP_NUMB_BITS: u64 = 64;
+
 /// A signed BigInteger with constant-time arithmetic.
 pub struct BigInteger {
-    inner: mpz_t
+    inner: mpz_t,
+    supposed_size: i64,
 }
 
 impl Display for BigInteger {
@@ -20,8 +24,8 @@ impl Display for BigInteger {
 }
 
 impl BigInteger {
-    fn init(size: i32) -> Self {
-        Self::zero(size as u64 * 64)
+    fn init(size: i64) -> Self {
+        Self::zero(size as u64 * GMP_NUMB_BITS)
     }
 
     /// Creates a BigInteger with value 0. All arithmetic operations are constant-time with regards to the integer's size `bits`.
@@ -31,7 +35,9 @@ impl BigInteger {
             gmp::mpz_init2(z.as_mut_ptr(), bits);
             let mut z = z.assume_init();
             z.size = z.alloc;  // Maximize the size given allocation
-            BigInteger { inner: z }
+            gmp::mpn_zero(z.d.as_mut(), z.size as i64);
+            //z.size = z.alloc;  // Maximize the size given allocation
+            BigInteger { inner: z, supposed_size: (bits / GMP_NUMB_BITS) as i64 }
         }
     }
 
@@ -41,9 +47,12 @@ impl BigInteger {
             let mut z = MaybeUninit::uninit();
             gmp::mpz_init2(z.as_mut_ptr(), bits);
             let mut z = z.assume_init();
-            gmp::mpz_set_ui(&mut z, value);
+            //mpz_limbs_write
             z.size = z.alloc;  // Maximize the size given allocation
-            BigInteger { inner: z }
+            gmp::mpn_zero(z.d.as_mut(), z.size as i64);
+            gmp::mpz_set_ui(&mut z, value);
+            //z.size = z.alloc;  // Maximize the size given allocation
+            BigInteger { inner: z, supposed_size: (bits / GMP_NUMB_BITS) as i64 }
         }
     }
 
@@ -60,51 +69,92 @@ impl BigInteger {
             gmp::mpz_get_ui(&self.inner)
         }
     }
-}
 
-impl AddAssign for BigInteger {
-    fn add_assign(&mut self, rhs: Self) {
-        unsafe {
-            gmp::mpn_add_n(self.inner.d.as_ptr(), self.inner.d.as_ptr(), rhs.inner.d.as_ptr(), (self.inner.size + rhs.inner.size) as i64);
-        }
-    }
-}
+    /// Compute `self` to the power `exponent` modulo `modulus`. The computation is constant in run time with regards to `self` and `exponent`, but variable with the size of `modulus`.
+    pub fn pow_mod(&self, exponent: &BigInteger, modulus: &BigInteger) -> BigInteger {
+        let mut result = BigInteger::init(modulus.supposed_size);
 
-impl Mul for BigInteger {
-    type Output = BigInteger;
-
-    fn mul(self, rhs: Self) -> Self::Output {
-        if rhs.inner.size > self.inner.size {
-            return rhs * self
-        }
+        let enb = exponent.supposed_size as u64 * GMP_NUMB_BITS;
 
         unsafe {
-            let mut result = BigInteger::init(self.inner.size + rhs.inner.size);
-
-            let scratch_size = gmp::mpn_sec_mul_itch(self.inner.size as i64, rhs.inner.size as i64) as usize;
-            println!("scratch size: {}", scratch_size);
+            let scratch_size = gmp::mpn_sec_powm_itch(self.supposed_size, enb, modulus.inner.size as i64) as usize * GMP_NUMB_BITS as usize;
 
             if scratch_size == 0 {
-                gmp::mpn_sec_mul(
+                gmp::mpn_sec_powm(
                     result.inner.d.as_mut(),
                     self.inner.d.as_ptr(),
-                    self.inner.size as i64,
-                    rhs.inner.d.as_ptr(),
-                    rhs.inner.size as i64,
+                    self.supposed_size,
+                    exponent.inner.d.as_ptr(),
+                    enb,
+                    modulus.inner.d.as_ptr(),
+                    modulus.inner.size as i64,
                     null_mut());
 
                 return result
             }
             
-            let scratch_layout = Layout::from_size_align(scratch_size, scratch_size).unwrap();
+            let scratch_layout = Layout::from_size_align(scratch_size, ALIGN).unwrap();
+            let scratch =  System.alloc(scratch_layout);
+
+            gmp::mpn_sec_powm(
+                result.inner.d.as_mut(),
+                self.inner.d.as_ptr(),
+                self.supposed_size,
+                exponent.inner.d.as_ptr(),
+                enb,
+                modulus.inner.d.as_ptr(),
+                modulus.inner.size as i64,
+                scratch as *mut u64);
+
+            System.dealloc(scratch, scratch_layout);
+
+            result
+        }
+    }
+}
+
+impl AddAssign for BigInteger {
+    fn add_assign(&mut self, rhs: Self) {
+        unsafe {
+            gmp::mpn_add_n(self.inner.d.as_ptr(), self.inner.d.as_ptr(), rhs.inner.d.as_ptr(), self.supposed_size + rhs.supposed_size);
+        }
+    }
+}
+
+impl Mul for &BigInteger {
+    type Output = BigInteger;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        if rhs.supposed_size > self.supposed_size {
+            return rhs * self
+        }
+
+        let mut result = BigInteger::init(self.supposed_size + rhs.supposed_size);
+        
+        unsafe {
+            let scratch_size = gmp::mpn_sec_mul_itch(self.supposed_size, rhs.supposed_size) as usize * GMP_NUMB_BITS as usize;
+
+            if scratch_size == 0 {
+                gmp::mpn_sec_mul(
+                    result.inner.d.as_mut(),
+                    self.inner.d.as_ptr(),
+                    self.supposed_size,
+                    rhs.inner.d.as_ptr(),
+                    rhs.supposed_size,
+                    null_mut());
+
+                return result
+            }
+            
+            let scratch_layout = Layout::from_size_align(scratch_size, ALIGN).unwrap();
             let scratch =  System.alloc(scratch_layout);
 
             gmp::mpn_sec_mul(
                 result.inner.d.as_mut(),
                 self.inner.d.as_ptr(),
-                self.inner.size as i64,
+                self.supposed_size,
                 rhs.inner.d.as_ptr(),
-                rhs.inner.size as i64,
+                rhs.supposed_size,
                 scratch as *mut u64);
 
             System.dealloc(scratch, scratch_layout);
@@ -146,7 +196,7 @@ mod tests {
         let a = BigInteger::new(23, 128);
         let b = BigInteger::new(14, 128);
 
-        let c = a * b;
+        let c = &a * &b;
 
         assert_eq!(23 * 14, c.get_u64());
     }
@@ -156,7 +206,7 @@ mod tests {
         let a = BigInteger::new(3, 128);
         let b = BigInteger::new(102, 64);
 
-        let c = a * b;
+        let c = &a * &b;
 
         assert_eq!(3 * 102, c.get_u64());
     }
@@ -166,8 +216,30 @@ mod tests {
         let a = BigInteger::new(12, 64);
         let b = BigInteger::new(25, 128);
 
-        let c = a * b;
+        let c = &a * &b;
 
         assert_eq!(12 * 25, c.get_u64());
+    }
+
+    #[test]
+    fn test_powexp() {
+        let b = BigInteger::new(1234, 128);
+        let e = BigInteger::new(23456, 128);
+        let m = BigInteger::new(999331, 128);
+
+        let res = b.pow_mod(&e, &m);
+
+        assert_eq!(465797, res.get_u64());
+    }
+
+    #[test]
+    fn test_powexp_small() {
+        let b = BigInteger::new(3, 128);
+        let e = BigInteger::new(2, 128);
+        let m = BigInteger::new(13, 128);
+
+        let res = b.pow_mod(&e, &m);
+
+        assert_eq!(9, res.get_u64());
     }
 }
