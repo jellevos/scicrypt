d667e6d1465850e7c20e87a3246ea19313436aa5
Jelle Vos <github@jelle-vos.nl>
2022-09-07 20:21:39 +0200

    Implement additive homomorphic operations for references

--- scicrypt-traits/src/homomorphic.rs
+++ scicrypt-traits/src/homomorphic.rs
@@ -11,81 +11,81 @@ pub trait HomomorphicAddition: EncryptionKey {
     /// Combines two ciphertexts so that their decrypted value reflects some addition operation
     fn add(
         &self,
-        ciphertext_a: Self::Ciphertext,
-        ciphertext_b: Self::Ciphertext,
+        ciphertext_a: &Self::Ciphertext,
+        ciphertext_b: &Self::Ciphertext,
     ) -> Self::Ciphertext;
 
     /// Combines two ciphertexts so that their decrypted value reflects some subtract operation
     fn sub(
         &self,
-        ciphertext_a: Self::Ciphertext,
-        ciphertext_b: Self::Ciphertext,
+        ciphertext_a: &Self::Ciphertext,
+        ciphertext_b: &Self::Ciphertext,
     ) -> Self::Ciphertext;
 
     /// Applies some operation on a ciphertext so that the decrypted value reflects some multiplication with `input`
-    fn mul_constant(&self, ciphertext: Self::Ciphertext, input: Self::Input) -> Self::Ciphertext;
+    fn mul_constant(&self, ciphertext: &Self::Ciphertext, input: &Self::Input) -> Self::Ciphertext;
 
     /// Combines two ciphertexts so that their decrypted value reflects some addition operation with a constant
     fn add_constant(
         &self,
-        ciphertext: Self::Ciphertext,
+        ciphertext: &Self::Ciphertext,
         constant: &Self::Plaintext,
     ) -> Self::Ciphertext;
 
     /// Combines two ciphertexts so that their decrypted value reflects some subtract operation with a constant
     fn sub_constant(
         &self,
-        ciphertext: Self::Ciphertext,
+        ciphertext: &Self::Ciphertext,
         constant: &Self::Plaintext,
     ) -> Self::Ciphertext;
 }
 
 impl<'pk, C: Associable<PK>, PK: EncryptionKey<Ciphertext = C> + HomomorphicAddition> Add
-    for AssociatedCiphertext<'pk, C, PK>
+    for &AssociatedCiphertext<'pk, C, PK>
 {
     type Output = AssociatedCiphertext<'pk, C, PK>;
 
     fn add(self, rhs: Self) -> Self::Output {
         debug_assert_eq!(self.public_key, rhs.public_key);
         self.public_key
-            .add(self.ciphertext, rhs.ciphertext)
+            .add(&self.ciphertext, &rhs.ciphertext)
             .associate(self.public_key)
     }
 }
 
-impl<'pk, C: Associable<PK>, PK: EncryptionKey<Ciphertext = C> + HomomorphicAddition>
-    Add<&PK::Plaintext> for AssociatedCiphertext<'pk, C, PK>
+impl<'pk, P: PotentialInput, C: Associable<PK>, PK: EncryptionKey<Ciphertext = C, Plaintext = P> + HomomorphicAddition>
+    Add<&P> for &AssociatedCiphertext<'pk, C, PK>
 {
     type Output = AssociatedCiphertext<'pk, C, PK>;
 
     fn add(self, rhs: &PK::Plaintext) -> Self::Output {
         self.public_key
-            .add_constant(self.ciphertext, rhs)
+            .add_constant(&self.ciphertext, rhs)
             .associate(self.public_key)
     }
 }
 
 impl<'pk, C: Associable<PK>, PK: EncryptionKey<Ciphertext = C> + HomomorphicAddition> Sub
-    for AssociatedCiphertext<'pk, C, PK>
+    for &AssociatedCiphertext<'pk, C, PK>
 {
     type Output = AssociatedCiphertext<'pk, C, PK>;
 
     fn sub(self, rhs: Self) -> Self::Output {
         debug_assert_eq!(self.public_key, rhs.public_key);
         self.public_key
-            .sub(self.ciphertext, rhs.ciphertext)
+            .sub(&self.ciphertext, &rhs.ciphertext)
             .associate(self.public_key)
     }
 }
 
-impl<'pk, C: Associable<PK>, PK: EncryptionKey<Ciphertext = C> + HomomorphicAddition>
-    Sub<&PK::Plaintext> for AssociatedCiphertext<'pk, C, PK>
+impl<'pk, P: PotentialInput, C: Associable<PK>, PK: EncryptionKey<Ciphertext = C, Plaintext = P> + HomomorphicAddition>
+    Sub<&P> for &AssociatedCiphertext<'pk, C, PK>
 {
     type Output = AssociatedCiphertext<'pk, C, PK>;
 
     fn sub(self, rhs: &PK::Plaintext) -> Self::Output {
         self.public_key
-            .sub_constant(self.ciphertext, rhs)
+            .sub_constant(&self.ciphertext, rhs)
             .associate(self.public_key)
     }
 }
@@ -95,13 +95,13 @@ impl<
         P: PotentialInput,
         C: Associable<PK>,
         PK: EncryptionKey<Input = P, Ciphertext = C> + HomomorphicAddition,
-    > Mul<P> for AssociatedCiphertext<'pk, C, PK>
+    > Mul<&P> for &AssociatedCiphertext<'pk, C, PK>
 {
     type Output = AssociatedCiphertext<'pk, C, PK>;
 
-    fn mul(self, rhs: PK::Input) -> Self::Output {
+    fn mul(self, rhs: &PK::Input) -> Self::Output {
         self.public_key
-            .mul_constant(self.ciphertext, rhs)
+            .mul_constant(&self.ciphertext, rhs)
             .associate(self.public_key)
     }
 }
