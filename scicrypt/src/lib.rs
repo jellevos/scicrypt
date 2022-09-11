81bb13c58a727a51d788d48772fe298a85701546
Jelle Vos <github@jelle-vos.nl>
2022-09-11 09:40:16 +0200

    Re-export bigint in the main crate

--- scicrypt/src/lib.rs
+++ scicrypt/src/lib.rs
@@ -1,6 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![warn(missing_docs, unused_imports)]
 
+pub use scicrypt_bigint;
 pub use scicrypt_he::cryptosystems;
 pub use scicrypt_he::threshold_cryptosystems;
 pub use scicrypt_numbertheory;
